cf4a44a67eb314ff1c60901d187b401d0d77faa6
AKurzman <32779938+AKurzman@users.noreply.github.com>
2017-10-14 00:28:58 -0700

    Update Cards.h
    Added member functions to all three classes to finish declarations

--- cards.h
+++ cards.h
@@ -7,74 +7,90 @@
 
 using namespace std;
 
-enum suit_t {OROS, COPAS, ESPADAS, BASTOS};
+enum suit_t { OROS, COPAS, ESPADAS, BASTOS };
 
-   /*
-   The values for this type start at 0 and increase by one 
-   afterwards until they get to SIETE. 
-   The rank reported by the function Card::get_rank() below is 
-   the value listed here plus one.
-   E.g: 
-   The rank of AS is reported as    static_cast<int>(AS) + 1   = 0 + 1 =  1
-   The rank of SOTA is reported as  static_cast<int>(SOTA) + 1 = 9 + 1 = 10
-   */
-enum rank_t {AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA=9, CABALLO=10, REY=11};
+/*
+The values for this type start at 0 and increase by one
+afterwards until they get to SIETE.
+The rank reported by the function Card::get_rank() below is
+the value listed here plus one.
+E.g:
+The rank of AS is reported as    static_cast<int>(AS) + 1   = 0 + 1 =  1
+The rank of SOTA is reported as  static_cast<int>(SOTA) + 1 = 9 + 1 = 10
+*/
+enum rank_t { AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA = 9, CABALLO = 10, REY = 11 };
 
 class Card {
-   public: 
-      // Constructor assigns random rank & suit to card.
-      Card();
-      
-      // Accessors 
-      string get_spanish_suit() const;
-      string get_spanish_rank() const; 
+public:
+	// Constructor assigns random rank & suit to card.
+	Card();
 
-      /* 
-         These are the only functions you'll need to code 
-         for this class. See the implementations of the two 
-	 functions above to get an idea of how to proceed.
-      */
-      string get_english_suit() const;
-      string get_english_rank() const; 
+	// Accessors 
+	string get_spanish_suit() const;
+	string get_spanish_rank() const;
 
-      // Converts card rank to number.
-      // The possible returns are: 1, 2, 3, 4, 5, 6, 7, 10, 11 and 12
-      int get_rank() const;
+	/*
+	These are the only functions you'll need to code
+	for this class. See the implementations of the two
+	functions above to get an idea of how to proceed.
+	*/
+	string get_english_suit() const;
+	string get_english_rank() const;
 
-      // Compare rank of two cards. E.g: Eight<Jack is true.
-      // Assume Ace is always 1. 
-      // Useful if you want to sort the cards.
-      bool operator < (Card card2) const;
+	// Converts card rank to number.
+	// The possible returns are: 1, 2, 3, 4, 5, 6, 7, 10, 11 and 12
+	int get_rank() const;
+
+	// Compare rank of two cards. E.g: Eight<Jack is true.
+	// Assume Ace is always 1. 
+	// Useful if you want to sort the cards.
+	bool operator < (Card card2) const;
+	double value();
 
 private:
-      suit_t suit;
-      rank_t rank;
+	suit_t suit;
+	rank_t rank;
 };
 
 
 class Hand {
-   public:
-      // A vector of Cards
-      Hand();
+public:
+	// A vector of Cards
+	Hand();
+	int numCards() { return int(myCards.size()); }
+	void addCard(Card NewCard);
+	double getpoints() { return count; }
+	void printCards();
+	void erase();
 
-      // You decide what functions you'll need...
+	// You decide what functions you'll need...
 
-   private:
-      // You decide what fields you'll need...
+private:
+	vector<Card>myCards;
+	double count = 0.0;
 };
 
 
 class Player {
-   public:
-      // Constructor. 
-      //    Assigns initial amount of money
-      Player(int m);
+public:
+	// Constructor. 
+	//    Assigns initial amount of money
+	Player(int m);
+	void addCard(Card newCard);
 
-      // You decide what functions you'll need...
+	int getMoney() { return money; };
+	void lose(int lost) { money -= lost; }
+	void win(int won) { money += won; }
+	Hand* getHand() { return &playerHand; }
+	void printCards();
+	void newHand();
 
-   private:
-      int money;
-      // You decide what extra fields (if any) you'll need...
+	// You decide what functions you'll need...
+
+private:
+	int money;
+	// You decide what extra fields (if any) you'll need...
+	Hand playerHand;
 };
 
-#endif
+#endif#pragma once
